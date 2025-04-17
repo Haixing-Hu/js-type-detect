e3aab863b2313d2065a0d80129feb81ae25ad42a
Haixing Hu <starfish.hu@gmail.com>
2025-04-17 21:51:41 +0800

    style: fix coding style error

--- src/is-iterator.js
+++ src/is-iterator.js
@@ -1,4 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
+import hasToStringValueOf from './impl/has-to-string-value-of';
 //
 //    Copyright (c) 2022 - 2024.
 //    Haixing Hu, Qubit Co. Ltd.
@@ -7,7 +8,6 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 import ITERATOR_TYPE_NAMES from './impl/iterator-type-names';
-import hasToStringValueOf from './impl/has-to-string-value-of';
 
 /**
  * Tests whether the specified value is a built-in iterator object.
@@ -23,22 +23,22 @@ function isIterator(value) {
   // first check if it is a built-in iterator type
   if (hasToStringValueOf(value, ITERATOR_TYPE_NAMES)) {
     return true;
-  }  
+  }
   // then check if it conforms to the iterator protocol
   if (value !== null && typeof value === 'object') {
     if (typeof value.next === 'function') {
       try {
         const result = value.next();
-        return result !== null 
-               && typeof result === 'object'
-               && 'done' in result
-               && 'value' in result;
+        return (result !== null)
+          && (typeof result === 'object')
+          && ('done' in result)
+          && ('value' in result);
       } catch (e) {
         // if the next method throws an error, it is not a valid iterator
         return false;
       }
     }
-  }  
+  }
   return false;
 }
 
